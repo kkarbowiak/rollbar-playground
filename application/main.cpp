697edcac3b73a02b8362dbe802ec5226d53f7444
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2023-01-18 13:40:54 +0100

    Extract args parsing to separate function

--- application/main.cpp
+++ application/main.cpp
@@ -1,11 +1,18 @@
 #include "argparse.h"
 
 
-int main(int argc, char * argv[])
+auto parse_args(int argc, char const * const argv[])
 {
     auto parser = argparse::ArgumentParser();
     parser.add_argument("data").help("data to send");
     parser.add_argument("--access-token").help("access token");
-    auto args = parser.parse_args(argc, argv);
+    return parser.parse_args(argc, argv);
+}
+
+auto main(int argc, char * argv[]) -> int
+{
+    auto const args = parse_args(argc, argv);
     (void) args;
+
+    return 0;
 }
